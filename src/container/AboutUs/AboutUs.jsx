d5100f58e3686c35a59626013824751650b2447a
Ankush <ankushcswork@gmail.com>
2022-05-28 16:06:22 +0530

    initial commit

---
+++ src/container/AboutUs/AboutUs.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+
+import "./AboutUs.css";
+import { images } from "../../constants";
+import Fade from "react-reveal/Fade";
+
+const AboutUs = () => (
+  <div
+    className="app__aboutus app__bg flex__center section__padding"
+    id="about"
+  >
+    <div className="app__aboutus-overlay flex__center">
+      <img src={images.G} alt="G letter" />
+    </div>
+
+    <div className="app__aboutus-content flex__center">
+      <div className="app__aboutus-content_about">
+        <Fade>
+          <h1 className="headtext__cormorant">About Us</h1>
+          <img alt="spoon" src={images.spoon} className="spoon__img" />
+          <p className="p__opensans">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis
+            pharetra adipiscing ultrices vulputate posuere tristique. In sed
+            odio nec aliquet eu proin mauris et.
+          </p>
+          <button type="button" className="custom__button">
+            Know More
+          </button>
+        </Fade>
+      </div>
+
+      <div className="app__aboutus-content_knife flex__center">
+        <img src={images.knife} alt="knife" />
+      </div>
+
+      <div className="app__aboutus-content_history">
+        <Fade>
+          <h1 className="headtext__cormorant">Our history</h1>
+          <img alt="spoon" src={images.spoon} className="spoon__img" />
+          <p className="p__opensans">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis
+            pharetra adipiscing ultrices vulputate posuere tristique. In sed
+            odio nec aliquet eu proin mauris et.
+          </p>
+          <button type="button" className="custom__button">
+            Know More
+          </button>
+        </Fade>
+      </div>
+    </div>
+  </div>
+);
+
+export default AboutUs;
